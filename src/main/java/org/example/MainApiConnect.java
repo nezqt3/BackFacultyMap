d7abb77651dfe35f89e7c960a4d05d82e5de515b
Denis <dalekseenko8@gmail.com>
2025-12-04 20:51:50 +0300

    init spring and add jsons

---
+++ src/main/java/org/example/MainApiConnect.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class MainApiConnect {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
